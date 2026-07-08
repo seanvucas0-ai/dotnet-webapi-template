33fec223042f07f8397d869382d0bf2dd3afe9d8
seanvucas0-ai <seanvucas0@gmail.com>
2026-07-08 12:35:01 +1000

    Update SampleController.cs

--- src/ServiceName/Controllers/SampleController.cs
+++ src/ServiceName/Controllers/SampleController.cs
@@ -1,1 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
 
+[ApiController]
+[Route("api/[controller]")]
+public class SampleController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World");
+    }
+}
